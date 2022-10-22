5b862425ccb92a2518841d0a2a8576c6ed3dfaf2
roopeh <44685964+roopeh@users.noreply.github.com>
2022-10-22 17:58:33 +0300

    Create layout for profile cover photo

--- frontend/src/components/ProfilePage.tsx
+++ frontend/src/components/ProfilePage.tsx
@@ -1,37 +1,41 @@
 import React from "react";
 
-const ProfilePage = () => (
-  <div>
-    <div className="profileBackground" />
-    <div className="profileCoverPhoto" />
+const ProfilePage = () => {
+  const root = document.querySelector(":root") as HTMLElement;
+  root.style.setProperty("--profileCoverPhotoHeight", "400px");
+  return (
+    <div>
+      <div className="profileBackground" />
+      <div className="profileCoverPhoto" />
 
-    <div className="profileContainer">
-      <div className="profileTopBar">
+      <div className="profileContainer">
+        <div className="profileTopBar">
 
-        <div className="profilePicture">
-          <div className="picture">
-            profile picture
+          <div className="profilePicture">
+            <div className="picture">
+              profile picture
+            </div>
+            <div>
+              edit profile
+            </div>
           </div>
-          <div>
-            edit profile
+
+          <div className="textContainer">
+            <div className="fillerFlex" />
+            <div style={{ flex: 4 }}>
+              Username
+              <br />
+              Firstname lastname
+            </div>
+            <div>
+              Some buttons here
+            </div>
           </div>
         </div>
-
-        <div className="textContainer">
-          <div className="fillerFlex" />
-          <div style={{ flex: 4 }}>
-            Username
-            <br />
-            Firstname lastname
-          </div>
-          <div>
-            Some buttons here
-          </div>
-        </div>
+        Content here...
       </div>
-      Content here...
     </div>
-  </div>
-);
+  );
+};
 
 export default ProfilePage;
