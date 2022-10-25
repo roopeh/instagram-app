6c4f4b68c632228f12de21fc268df013df62d3b6
roopeh <44685964+roopeh@users.noreply.github.com>
2022-10-25 03:10:55 +0300

    Finish layout for profile page

--- frontend/src/components/ProfilePage/ProfilePageContent.tsx
+++ frontend/src/components/ProfilePage/ProfilePageContent.tsx
@@ -3,47 +3,63 @@ import {
   Routes, Route, useNavigate,
 } from "react-router-dom";
 
+import musk from "../../assets/placeholders/musk.jpg";
+
 interface ProfileContentProps {
-  coverPhotoFunc: any
+  coverPhotoFunc: () => void
 }
 
 const ProfilePageContent = ({ coverPhotoFunc }: ProfileContentProps) => {
   const navigate = useNavigate();
   return (
-    <div>
-      Content here...
-      <br />
-      <button
-        type="button"
-        onClick={() => navigate("123")}
-      >
-        test overlay
-      </button>
-      <br />
-      <button
-        type="button"
-        onClick={coverPhotoFunc}
-      >
-        test cover photo
-      </button>
+    <div className="profilePage__mainContent">
+      <div style={{ flex: "0 0 80px" }}>
+        <button
+          type="button"
+          onClick={() => navigate("123")}
+        >
+          test overlay
+        </button>
+        <br />
+        <button
+          type="button"
+          onClick={coverPhotoFunc}
+        >
+          test cover photo
+        </button>
 
-      <Routes>
-        <Route
-          path="/:photoId"
-          element={(
-            <div
-              aria-hidden="true"
-              className="profilePage__imageModal"
-              onClick={() => navigate(-1)}
-              onKeyDown={() => navigate(-1)}
-            >
-              <div>
-                Test modal
+        <Routes>
+          <Route
+            path="/:photoId"
+            element={(
+              <div
+                aria-hidden="true"
+                className="profilePage__imageModal"
+                onClick={() => navigate(-1)}
+                onKeyDown={() => navigate(-1)}
+              >
+                <div>
+                  Test modal
+                </div>
+              </div>
+          )}
+          />
+        </Routes>
+      </div>
+      <div className="profilePage__imageFlex">
+        {[...Array(9)].map((e, i) => (
+          <div key={e} className="profilePage__imageFlex__rowDivider">
+            <div className="profilePage__imageContainer">
+              <div className="profilePage__imageContainer__dateText">
+                {(i % 2) ? null : "October 2022"}
+              </div>
+              <div className="profilePage__imageContainer__imageBox">
+                <img src={musk} alt="Musk" />
               </div>
             </div>
-          )}
-        />
-      </Routes>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
