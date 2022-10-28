232b93b407d22039ccc295e3fb6f402dcea598ea
roopeh <44685964+roopeh@users.noreply.github.com>
2022-10-28 05:02:50 +0300

    Backend: Add some graphql schemas

--- backend/src/graphql/schemas.ts
+++ backend/src/graphql/schemas.ts
@@ -1,8 +1,29 @@
 const { gql } = require("apollo-server");
 
+// TODO: photo comments, user private messages, user followers/following, paging
 const typeDefs = gql`
+  type Photo {
+    id: ID!
+    imageString: String!
+    publishDate: String!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    password: String!
+    firstName: String!
+    lastName: String!
+    bioText: String
+    profilePhoto: [Photo!]!
+    coverPhoto: [Photo!]
+    photos: [Photo!]
+    photoCount: Int!
+  }
+
   type Query {
     userCount: Int!
+    allUsers: [User!]!
   }
 `;
 
